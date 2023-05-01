01d761527872b1090897841bdd5b62186599d4ee
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-05-01 15:40:24 -0500

    Create the seed data: five greetings.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Message.create(message: "Hello, everyone! I'm so happy to see you all here today. Thank you for joining me in this wonderful event. I hope you are having a great time and enjoying the activities.")
+
+Message.create(message: "Hi there! Welcome to this amazing place. I'm glad you decided to come and explore with me. There are so many things to see and do here. Let's have some fun!")
+
+Message.create(message: "Greetings, friends! It's a pleasure to meet you all. You are in for a treat today. I have prepared something special for you. I'm sure you will love it as much as I do.")
+  
+Message.create(message: "Good day, folks! How are you doing? I'm excited to share with you some of the secrets of this place. You will be amazed by what you will discover. Are you ready?")
+
+Message.create(message: "Salutations, comrades! I salute you for your courage and curiosity. You have come to the right place. This is where the magic happens. Let's get started!")
+
+
+
+
