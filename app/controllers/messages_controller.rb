8ba9a610601e6a5d4fda7c9bfac48eabc3a06fa2
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-05-02 15:14:55 -0500

    Ensuring the next random message is different.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,17 @@
 class MessagesController < ApplicationController
+  @last_msg_id = 0
   before_action :set_message, only: %i[show edit update destroy]
 
   # GET /messages or /messages.json
   def index; end
 
   def random
-    @random_msg = Message.all.sample
+    begin
+      @random_msg = Message.all.sample
+    end until @last_msg_id != @random_msg.id
 
+    @last_msg_id = @random_msg.id
+    
     respond_to do |format|
       format.json { render json: @random_msg }
     end
