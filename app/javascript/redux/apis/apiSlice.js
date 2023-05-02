d4993172e9b40eaf4684c250aca2cc48bce28e9a
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-05-02 12:34:33 -0500

    Create apiSlice.

---
+++ app/javascript/redux/apis/apiSlice.js
@@ -0,0 +1,21 @@
+// Import the RTK Query methods from the React-specific entry point
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+// Define our single API slice object
+export const apiSlice = createApi({
+  // The cache reducer expects to be added at `state.api` (already default - this is optional)
+  reducerPath: 'api',
+  // All of our requests will have URLs starting with '/'
+  baseQuery: fetchBaseQuery({ baseUrl: '/' }),
+  // The "endpoints" represent operations and requests for this server
+  endpoints: builder => ({
+    // The `getMessage` endpoint is a "query" operation that returns data
+    getMessage: builder.query({
+      // The URL for the request is '/random'
+      query: () => '/random.json'
+    })
+  })
+})
+
+// Export the auto-generated hook for the `getMessage` query endpoint
+export const { useGetMessageQuery } = apiSlice
